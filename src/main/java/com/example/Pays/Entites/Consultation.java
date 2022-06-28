725c2d87ed6ec84e0acf929d4e09dc92c7a483ba
ndieric <ndihokubwayoeric26@gmail.com>
2022-06-28 17:51:02 +0200

    modifconsultation

--- src/main/java/com/example/Pays/Entites/Consultation.java
+++ src/main/java/com/example/Pays/Entites/Consultation.java
@@ -5,6 +5,12 @@ import java.util.Date;
 public class Consultation {
     private int id_consultation;
     private Date date;
+    public Consultation(int id_consultation, Date date) {
+        this.id_consultation = id_consultation;
+        this.date = date;
+    }
+    
+    
     
 
     
